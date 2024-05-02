c8ed5261371b481ba60444c805d5ee9632a34c53
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-05-02 14:38:11 +0530

    ✨ using ES6 modules

---
+++ ES6/Modules/calc.js
@@ -0,0 +1,4 @@
+import Addition, { Product, PI } from "./math.js";
+console.log("Addition is : " + Addition(20, 30));
+console.log("Multiplication is : " + Product(20, 30));
+console.log("Value of PI " + PI);
