d4c082c866c69e77fc1e51a439979aaba687dfe4
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-05-02 14:41:23 +0530

    ✨ importing everything (that's exported)

--- ES6/Modules/calc.js
+++ ES6/Modules/calc.js
@@ -1,4 +1,9 @@
-import Addition, { Product, PI } from "./math.js";
-console.log("Addition is : " + Addition(20, 30));
-console.log("Multiplication is : " + Product(20, 30));
-console.log("Value of PI " + PI);
+// import Addition, { Product, PI } from "./math.js";
+// console.log("Addition is : " + Addition(20, 30));
+// console.log("Multiplication is : " + Product(20, 30));
+// console.log("Value of PI " + PI);
+
+import * as MathModule from "./math.js";
+console.log("Addition is : " + MathModule.default(20, 30));
+console.log("Multiplication is : " + MathModule.Product(20, 30));
+
