d864d3636faa3867dbd2c4dfedf7fcd1bd040e37
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-05-07 14:23:31 +0530

    ✨ Handling Errors in AJAX

--- AJAX/AJAXwithXMLHttpReq/GetData.js
+++ AJAX/AJAXwithXMLHttpReq/GetData.js
@@ -1,9 +1,11 @@
 function GetData(callback) {
   var xmlHttpReq = new XMLHttpRequest();
-  xmlHttpReq.open("GET", "https://jsonplaceholder.typicode.com/posts");
+  xmlHttpReq.open("GET", "https://jsonplaceholder.typicode.com/postss");
   xmlHttpReq.onreadystatechange = function () {
-    if (xmlHttpReq.readyState == 4 && xmlHttpReq.status == 200) {
-      callback(JSON.parse(xmlHttpReq.responseText));
+    if (xmlHttpReq.readyState === 4 && xmlHttpReq.status === 200) {
+      callback(JSON.parse(xmlHttpReq.responseText), null);
+    } else if (xmlHttpReq.readyState === 4 && xmlHttpReq.status !== 200) {
+      callback(null, `Something went wrong - ${xmlHttpReq.status} !`);
     }
   };
   xmlHttpReq.send(); // places the async call
