5951a2519b3223fdc648e97ecd2285f4d41d2fda
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-05-07 15:21:54 +0530

    ✨ using promises | using axios

--- AJAX/AJAXwithPromises/GetData.js
+++ AJAX/AJAXwithPromises/GetData.js
@@ -1,16 +1,14 @@
 function GetData() {
   return new Promise((resolve, reject) => {
-    setTimeout(() => resolve("Success"), 3000);
+    var xmlHttpReq = new XMLHttpRequest();
+    xmlHttpReq.open("GET", "https://jsonplaceholder.typicode.com/posts");
+    xmlHttpReq.onreadystatechange = function () {
+      if (xmlHttpReq.readyState === 4 && xmlHttpReq.status === 200) {
+        resolve(JSON.parse(xmlHttpReq.responseText));
+      } else if (xmlHttpReq.readyState === 4 && xmlHttpReq.status !== 200) {
+        reject(xmlHttpReq.status);
+      }
+    };
+    xmlHttpReq.send(); // places the async call
   });
 }
-
-// var xmlHttpReq = new XMLHttpRequest();
-//   xmlHttpReq.open("GET", "https://jsonplaceholder.typicode.com/postss");
-//   xmlHttpReq.onreadystatechange = function () {
-//     if (xmlHttpReq.readyState === 4 && xmlHttpReq.status === 200) {
-//       callback(JSON.parse(xmlHttpReq.responseText), null);
-//     } else if (xmlHttpReq.readyState === 4 && xmlHttpReq.status !== 200) {
-//       callback(null, `Something went wrong - ${xmlHttpReq.status} !`);
-//     }
-//   };
-//   xmlHttpReq.send(); // places the async call
